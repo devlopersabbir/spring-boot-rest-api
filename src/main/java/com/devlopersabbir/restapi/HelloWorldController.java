60cbac2eb5e84ef4efcd6edff6a76e0f1c7256c6
devlopersabbir <devlopersabbir@gmail.com>
2023-09-12 01:45:26 +0600

    Hello World Message

---
+++ src/main/java/com/devlopersabbir/restapi/HelloWorldController.java
@@ -0,0 +1,11 @@
+package com.devlopersabbir.restapi;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path="/hello")
+    public String helloWorld(){
+        return "Hello World! Sabbir";
+    }
+}
